5c7d141348fb712639630a3aa195e6058afc7789
justynatom <justynaxtomaszewska@gmail.com>
2022-12-04 01:42:24 +0100

    completing code logic and adusting web gui elements

--- air_quality_app.py
+++ air_quality_app.py
@@ -1,6 +1,7 @@
 import api_talker
 import data_parser
 import matplotlib.pyplot as plt
+import os
 
 class Air_quality_app:
     def __init__(self):
@@ -9,22 +10,32 @@ class Air_quality_app:
         self.full_station_list = self.api_talker.send_api_request_findAll()
         self.stations_list = []
         self.sensors_list = []
+        self.station_name = ''
+        self.sensor_name = ''
+
+    def set_station_name(self, station):
+        self.station_name = station
+
+    def set_sensor_name(self, sensor):
+        self.sensor_name = sensor
 
     def plot_graph(self, dates, values):
+        plt.clf()
         plt.plot(dates, values)
 
         # naming the x axis
         plt.xlabel('x - axis')
         plt.tick_params(axis='x', rotation=85, labelsize=5)
         # naming the y axis
-        plt.ylabel('y - axis')
+        plt.ylabel(self.sensor_name)
 
         plt.grid(True, which='major')
 
         # giving a title to my graph
-        #plt.title('My first graph!')
+        plt.title(self.station_name + ': ' + self.sensor_name)
         plt.margins(x=0, y=0)
 
+        os.remove("static/plot.jpg")
         plt.savefig("static/plot.jpg", dpi=500)
         # function to show the plot
         #plt.show()
@@ -41,20 +52,30 @@ class Air_quality_app:
         dates, values = self.data_parser.get_date_and_sensors_values_lists(sensor_data)
         self.plot_graph(dates, values)
 
-    def test2(self):
-        stations = self.data_parser.search_stations_in_province("opolskie", self.full_station_list)
+    def get_stations_in_province(self, province):
+        stations = self.data_parser.search_stations_in_province(province, self.full_station_list)
         self.stations_list = stations
         return self.data_parser.get_stations_names_from_list(stations)
 
-    def print_sensors(self, id):
+    def get_sensors_in_station(self, id):
+        self.set_station_name(self.stations_list[id-1]['stationName'])
         station_id = self.data_parser.choose_station_and_find_station_id(self.stations_list, id)
 
-        self.sensors = self.api_talker.send_api_request_sensors(station_id)
-        return self.data_parser.get_sensor_names_from_list(self.sensors)
+        self.sensors_list = self.api_talker.send_api_request_sensors(station_id)
+        return self.data_parser.get_sensor_names_from_list(self.sensors_list)
 
 
-    def show_graph(self, id):
-        sensor_id = self.data_parser.choose_sensor_and_find_sensor_id(self.sensors, id)
+    def get_measurements_and_show_graph(self, id):
+        self.set_sensor_name(self.sensors_list[id-1]['param']['paramName'])
+        sensor_id = self.data_parser.choose_sensor_and_find_sensor_id(self.sensors_list, id)
         sensor_data = self.api_talker.send_api_request_getData(sensor_id)
         dates, values = self.data_parser.get_date_and_sensors_values_lists(sensor_data)
         self.plot_graph(dates, values)
+        numerical_data = []
+        for i in range(len(dates)):
+            numerical_data.append(str(dates[i]) + ": " + str(values[i]))
+        return numerical_data
+
+    def get_provinces_list(self):
+        province_list = ["Dolnośląskie", "Mazowieckie", "Wielkopolskie", "Lubuskie"]
+        return province_list
--- main.py
+++ main.py
@@ -12,12 +12,19 @@ obj = air_quality_app.Air_quality_app()
 app = FastAPI()
 app.counter = 0
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 class HelloResp(BaseModel):
     msg: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World...!"}
+@app.get("/",  response_class=HTMLResponse)
+async def root(request: Request):
+    #provinces = ["Dolnośląskie", "Mazowieckie", "Wielkopolskie"]
+    return templates.TemplateResponse("index.html", {"request": request, 'choices': obj.get_provinces_list()})
+
+@app.post("/")
+async def get_province(province: str = Form()):
+    print(province)
+    return RedirectResponse(url="/stations/" + str(province.title()), status_code=status.HTTP_302_FOUND)
 
 
 @app.get("/hello/{name}", response_model=HelloResp)
@@ -48,36 +55,33 @@ async def get_something(
 ):
     return {'selected': choice}
 
-templates = Jinja2Templates(directory="templates")
+
 
 
 @app.get("/test2/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
 
-@app.get("/test3", response_class=HTMLResponse)
-async def read_item(request: Request):
-    item = obj.test2()
-    province = "Opolskie"
-    return templates.TemplateResponse("item2.html", {"request": request, 'items': item, 'province': province})
+@app.get("/stations/{province}", response_class=HTMLResponse)
+async def read_item(request: Request, province: str):
+    item = obj.get_stations_in_province(province)
+    return templates.TemplateResponse("item2.html", {"request": request, 'items': item, 'province': province, 'min_value': 1, "max_value": len(item)})
 
 
-@app.get("/sensors/{sensor_id}", response_class=HTMLResponse)
-async def print_sensors(request: Request, sensor_id: str):
-    item = obj.print_sensors(int(sensor_id))
-    station = "test"
-    return templates.TemplateResponse("item3.html", {"request": request, 'items': item, 'station': station})
+@app.get("/sensors/{station_id}", response_class=HTMLResponse)
+async def print_sensors(request: Request, station_id: str):
+    item = obj.get_sensors_in_station(int(station_id))
+    return templates.TemplateResponse("item3.html", {"request": request, 'items': item, 'station': obj.station_name, 'min_value': 1, "max_value": len(item)})
 
-@app.post("/test3")
+@app.post("/stations/{province}")
 async def get_id(id: str = Form()):
     print(id)
     return RedirectResponse(url="/sensors/" + str(id), status_code=status.HTTP_302_FOUND)
 
 @app.get("/measurement/{measure}")
 async def show_measurement(request: Request, measure: str):
-    obj.show_graph(int(measure))
-    time.sleep(2)
-    return templates.TemplateResponse("item4.html", {"request": request})
+    data = obj.get_measurements_and_show_graph(int(measure))
+    return templates.TemplateResponse("item4.html", {"request": request, 'data': data})
 
 @app.post("/sensors/{sensor_id}")
 async def get_sensor(id2: str = Form()):
